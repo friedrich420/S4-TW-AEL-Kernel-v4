149531c5b1a6cbcb600c8ba9cce467e14d9311a3
friedrich420 <kelpidorou@yahoo.com>
2014-08-04 09:31:32 +0300

    Add auto_hotplug from thalamus

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -32,6 +32,9 @@
 
 #include <trace/events/power.h>
 
+static unsigned int Lenable_auto_hotplug = 1;
+extern void apenable_auto_hotplug(bool state);
+
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -628,6 +631,21 @@ static ssize_t show_vdd_levels(struct kobject *a, struct attribute *b, char *buf
 	return acpuclk_get_vdd_levels_str(buf);
 }
 
+static ssize_t show_enable_auto_hotplug(struct cpufreq_policy *policy, char *buf)
+{
+       return sprintf(buf, "%u\n", Lenable_auto_hotplug);
+}
+static ssize_t store_enable_auto_hotplug(struct cpufreq_policy *policy,
+ 				       const char *buf, size_t count)
+{
+       unsigned int val = 0;
+       unsigned int ret;
+       ret = sscanf(buf, "%u", &val);
+       Lenable_auto_hotplug = val;
+       apenable_auto_hotplug((bool) Lenable_auto_hotplug);
+       return count;
+}
+
 static ssize_t store_vdd_levels(struct kobject *a, struct attribute *b, const char *buf, size_t count) {
 
 	int i = 0, j;
@@ -696,6 +714,7 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+cpufreq_freq_attr_rw(enable_auto_hotplug);
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 define_one_global_rw(vdd_levels);
 #endif
@@ -713,6 +732,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	&enable_auto_hotplug.attr,
 	NULL
 };
 
