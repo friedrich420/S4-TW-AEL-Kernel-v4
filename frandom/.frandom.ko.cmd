e110573d9d0eb520abc63277c5aa90aafe69a6d4
friedrich420 <kelpidorou@yahoo.com>
2014-08-18 19:03:10 +0300

    recommit my lost git folder- crap crappers

---
+++ frandom/.frandom.ko.cmd
@@ -0,0 +1,1 @@
+cmd_frandom/frandom.ko := /home/friedrich420/kernel/Toolchain4.6/arm-eabi-4.6/bin/arm-eabi-ld -EL -r  -T /home/friedrich420/kernel/s4/NG2SOURCE/scripts/module-common.lds --build-id  -o frandom/frandom.ko frandom/frandom.o frandom/frandom.mod.o
